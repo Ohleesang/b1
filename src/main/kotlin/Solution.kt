338f6f08891d6ecabc2817a13d5a305349f4b48a
Ohleesang <jammmmed@gmail.com>
2023-12-20 11:08:38 +0900

    복합적인 문자열 분리 Regex 를 이용하여 알고리즘 풀이

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,20 +1,30 @@
 class Solution {
     fun solution(s: String): IntArray {
         var answer = intArrayOf()
-        val regex = Regex("[{}]+")
-        var stringBuilder = StringBuilder()
-        val list = mutableListOf<String>()
+        val regex = Regex("\\{(\\d+(?:,\\d+)*)\\}")
+        //  \{ ( \d+ (?:, \d+ )*) \}
+        // (A+B*)
+        //A : \d+ -> 하나이상의 숫자
+        //B : (?:,A) -> ,로 시작하고 그뒤에 하나이상의 숫자가 올경우
+        //  (?:X) --> 찾지만 그룹에 포함시키지 않음 ..{1,2,3} 을 원하지 {1,2,3},{2,3} 꼴을 원하진 않음
+        //B* : B가 0번이상 발생할수 있음
+        // '\{' -->'\{' 이스케이프!
 
-        s.forEach {
-            stringBuilder.append(it)
-            if (it == '}') {
-                var temp = stringBuilder.replace(regex,"")
 
-                list.add(temp)
-                stringBuilder.clear()
+        var matches = regex.findAll(s)
+        //groupValues[0] : 전체 정규표현식에 일치하는 부분 '(\d+(?:,\d+)*)'
+        //groupValues[1] : 첫번째 그룹에 해당되는 값 ,(?:X) 가 적용된 값들이 들어감.
+        var result = matches.map{it.groupValues[1].split(',').map(String::toInt)}.toMutableList()
+        result.sortBy{it.size}
+
+        //작은 숫자 부터 입력
+        var resultSet = mutableSetOf<Int>()
+        result.forEach{
+            it.forEach{ value ->
+                resultSet.add(value)
             }
         }
-
+        answer = resultSet.toIntArray()
         return answer
     }
 }
@@ -27,4 +37,5 @@ fun main() {
     a.solution("{{20,111},{111}}")//111,20
     a.solution("{123}")//123
     a.solution("{4,2,3},{3},{2,3,4,1},{2,3}")//3,2,4,1
+
 }
